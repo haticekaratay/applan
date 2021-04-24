9de61691303421b0f2acafa1ab7c3466c8aa1f04
Hatice <haticekaraty@gmail.com>
2021-04-24 00:03:23 -0400

    Adds flash error and success messages to sign up

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,10 +6,12 @@ class TeachersController < ApplicationController
 
     post "/signup" do
         if params[:username].empty? || params[:password].empty? || Teacher.find_by_username(params[:username])
+            flash[:error] = "Invalid user input! Try again."
             redirect "/signup"
         end
         teacher = Teacher.create(username: params[:username], password: params[:password])
         session[:teacher_id] = teacher.id  #adds a new key value pair to session hash
+        flash[:success] = "User is succesfully created.Please log in to system."
         redirect "/plans"   # this route will be created in controllers. 
     end
 
