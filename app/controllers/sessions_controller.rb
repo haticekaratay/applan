2feb1ac3d9c408bb9fd4bdc3e4e5ad01a4a9af61
Hatice <haticekaraty@gmail.com>
2021-04-24 00:32:15 -0400

    Adds login success and error flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
         teacher = Teacher.find_by_username(params[:username])
         if teacher && teacher.authenticate(params[:password])
             session[:teacher_id] = teacher.id
+            flash[:success] = "Successfully logged in!"
             redirect "/plans"
         else
+            flash[:error] = "Username and password doesn't match! or invalid entry!"
             redirect "/login"
         end
     end
 
     get "/logout" do
         session.clear
+        flash[:success] = "Successfully logged out! See you soon!"
         redirect "/login"
     end
 end
